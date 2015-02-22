d0128a6109895cfe0867b206db733e14504ceb38
Patrick Gaubatz <patrick.gaubatz@univie.ac.at>
2015-02-22 14:58:50 +0100

    fix project name

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,5 +1,5 @@
 #
-# Tvplexend - A Tvheadend Agent Plugin for PLEX Media Server
+# TvplexendAgent.bundle - A Tvheadend Agent Plugin for PLEX Media Server
 # Copyright (C) 2015 Patrick Gaubatz <patrick@gaubatz.at>
 #
 # This program is free software; you can redistribute it and/or modify
